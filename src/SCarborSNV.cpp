93eeb2191ae67deac5267709c5c78cfbdcdfe4ff
Christopher Oldham <coldham10@gmail.com>
2019-04-16 16:18:47 -0400

    All priors complete and checked up to P(sig|SNV_T, H)

--- src/SCarborSNV.cpp
+++ src/SCarborSNV.cpp
@@ -2,13 +2,21 @@
 #include <cmath>
 #include "sigma_priors.h"
 
-int main(void) {
-    std::cout << "Enter m: ";
+int main(int argc, char** argv) {
     int m;
-    std::cin >> m;
+    if (argc < 2 ) {
+        std::cout << "Enter m: ";
+        std::cin >> m;
+    }
+    else {
+        m = atoi(argv[1]);
+    }
+    double sum = 0;
     std::vector<double> l_P_sigmas = log_sigma_priors(m);
     for (std::vector<double>::iterator it = l_P_sigmas.begin(); it != l_P_sigmas.end(); ++it) {
-        std::cout << std::exp(*it) << std::endl;
+//        std::cout << std::exp(*it) << std::endl;
+        sum += std::exp(*it);
     }
+    std::cout << sum << std::endl;
     return 0;
 }
--- src/sigma_priors.cpp
+++ src/sigma_priors.cpp
@@ -5,7 +5,8 @@
 double log_prior(int m, int sig);
 long double log_factorial(int x);
 long double l_P_intree__somatic(int m, double l_P_clonal);
-long double l_P_sig__sSNV_NSNVT(int m, int sig, double l_P_H, long double l_P_HT__H);
+//long double l_P_sig__sSNV_NSNVT(int m, int sig, double l_P_H, long double l_P_HT__H);
+long double l_P_sig__SNVT_H(int m, int sig, long double l_P_HT__H);
 
 std::vector<double> log_sigma_priors(int m) {
     std::vector<double> l_prior_vec; 
@@ -20,7 +21,8 @@ std::vector<double> log_sigma_priors(int m) {
 double log_prior(int m, int sig) {
     //FIXME
     static long double l_P_tree = l_P_intree__somatic(m, std::log(0.51));
-    return l_P_sig__sSNV_NSNVT(m, sig, std::log(0.09), l_P_tree);
+    //return l_P_sig__sSNV_NSNVT(m, sig, std::log(0.09), l_P_tree);
+    return l_P_sig__SNVT_H(m, sig, l_P_tree);
 }
 
 long double log_factorial(int x) {
@@ -40,6 +42,26 @@ inline long double log_binom(int n, int k) {
 
 }
 
+long double LSE(std::vector<long double> to_sum) {
+    long double maxv = std::log(static_cast<long double>(0));
+    long double sum = 0;
+    for (std::vector<long double>::iterator it = to_sum.begin(); it != to_sum.end(); ++it) {
+        maxv = (*it > maxv) ? *it : maxv;
+    }
+    if (maxv == std::log(static_cast<long double>(0))) {
+        return maxv;
+    }
+    for (std::vector<long double>::iterator it = to_sum.begin(); it != to_sum.end(); ++it) {
+        //LSE 'trick'
+        sum += std::exp(*it - maxv);
+    }
+    return std::log(sum) + maxv;
+}
+
+inline long double LSE(long double arg1, long double arg2) {
+    std::vector<long double> v{arg1, arg2};
+    return LSE(v);
+}
 long double l_T(int a, int b) {
     //Kuipers et al. inspired tree function
     long double numerator = 2 * log_binom(a,b);
@@ -76,7 +98,6 @@ long double l_P_ancestral__subclonal(int m) {
 }
 
 long double l_P_intree__somatic(int m, double l_P_clonal) {
-    //TODO memoize
     long double P_clonal = std::exp(static_cast<long double>(l_P_clonal));
     long double l_P_ancestral = l_P_ancestral__subclonal(m) + std::log(1-P_clonal);
     //TODO logaddexp
@@ -100,7 +121,7 @@ long double l_P_sig__sSNV_NSNVT_HT(int m, int sig) {
 long double l_P_sig__sSNV_NSNVT_H(int m, int sig, long double l_P_HT__H) {
     long double term_1 = std::log(1-std::exp(l_P_HT__H)) + l_P_sig__sSNV_NSNVT_H_NHT(m, sig);
     long double term_2 = l_P_HT__H + l_P_sig__sSNV_NSNVT_HT(m, sig);
-    return std::log(std::exp(term_1) + std::exp(term_2));
+    return LSE(term_1, term_2);
 }
 
 inline long double l_P_sig__sSNV_NSNVT_NH(int m, int sig) {
@@ -110,6 +131,45 @@ inline long double l_P_sig__sSNV_NSNVT_NH(int m, int sig) {
 long double l_P_sig__sSNV_NSNVT(int m, int sig, double l_P_H, long double l_P_HT__H) {
     long double term_1 = l_P_sig__sSNV_NSNVT_H(m, sig, l_P_HT__H) + static_cast<long double>(l_P_H);
     long double term_2 = l_P_sig__sSNV_NSNVT_NH(m, sig) + std::log(1-std::exp(static_cast<long double>(l_P_H)));
-    //TODO logaddexp
-    return std::log(std::exp(term_1) + std::exp(term_2));
+    return LSE(term_1, term_2);
+}
+
+
+
+long double l_P_sig__SNVT_HT(int m, int sig) {
+    std::vector<long double> to_sum;
+    for (int a = 1; a < m; ++a) {
+        for (int h = 1; h < m; ++h) {
+            if (a-h == sig && h <= a) {
+                //Case 1
+                to_sum.push_back(l_T(m,a) + l_T(a,h));
+            }
+            if (a+h == sig && h <= a) {
+                //Case 2
+                to_sum.push_back(l_T(m,a) + l_T(a,h));
+            }
+            if (2*a == sig && h >= a) {
+                //Case 3
+                to_sum.push_back(l_T(m,h) + l_T(h,a));
+            }
+        } //for h
+    } //for a
+    long double l_sum = LSE(to_sum);
+    return l_sum - std::log(static_cast<long double>(3)) + std::log(2*m-1) - std::log(2*(m-1));
+}
+
+long double l_P_sig__SNVT_H_NHT(int m, int sig) {
+    if (sig > 0 && sig < 2*m && sig % 2 == 0) {
+        return std::log(static_cast<long double>(2*m - 1)) 
+            - std::log(static_cast<long double>(2*(m-1)))
+            + l_T(m, sig/2);
+    } else {
+        return std::log(static_cast<long double>(0));
+    }
+}
+
+long double l_P_sig__SNVT_H(int m, int sig, long double l_P_HT__H) {
+    long double term_1 = l_P_sig__SNVT_HT(m, sig) + l_P_HT__H;
+    long double term_2 = l_P_sig__SNVT_H_NHT(m, sig) + std::log(1-std::exp(l_P_HT__H));
+    return LSE(term_1, term_2);
 }
