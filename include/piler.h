5202880d677641c9433b942faa53500e2e8b0e24
Christopher Oldham <coldham10@gmail.com>
2019-05-27 11:35:49 -0400

    Fixed piler helper class issueswq

--- include/piler.h
+++ include/piler.h
@@ -3,24 +3,58 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <mutex>
 
+/* All the read and qual data from a single cell at a locus*/
+//TODO when are these allocated and deallocated? Make Locus_reads a class?
 struct Cell_reads {
     std::vector<read> reads;
     int n_reads;
     nuc_t ref;
 };
 
+typedef std::vector<Cell_reads> Locus_reads;
+
+/* A batch of 'batch_size' loci with the reads of each cell at those loci.
+ * Note Batches get made and analyzed by single threads so don't need mutexes
+ * */
 class Batch {
     public:
-        Batch(int batch_size);
+        /* Allocates memory for the batch*/
+        Batch(int batch_size, int id);
+        /*Returns size of this batch even if smaller than standard batch size*/
+        unsigned int get_batch_size();
+        int get_batch_id();
+        Locus_reads* get_locus(int i);
         ~Batch();
-        int get_batch_size();
-        Cell_reads get_cell(int i);
 
     private:
-        int batch_size;
-        Cell_reads* data;
+        int batch_size, batch_id;
+        Locus_reads** data;
 };
+
+/*Thread safe queue of batches ready for pickup by workers */
+class Batch_Q {
+    public:
+        Batch_Q();
+        void set_max_size(int max_size);
+        /*Returns oldest batch and deletes from queue
+         * If empty returns null pointer TODO:(?)*/
+        //TODO: mutex protected. If no batch available yet(and !read_complete), what to do? return null? Wait? reader only takes mutex when pushing complete Batch. 
+        Batch* pop();
+        void push(Batch* b);
+        unsigned int size();
+        ~Batch_Q();
+
+    private:
+        unsigned int max_size, n;
+        bool read_complete;
+        std::queue<Batch*> batch_queue;
+        //TODO initialize
+        std::mutex m_queue;
+};
+
 
 
 class Piler {
@@ -28,15 +62,15 @@ class Piler {
         /* Sets the read stream and starts working to populate batch list
          * */ 
         Piler(std::istream* instream, int batch_size=1000, bool is_bams=false);
+        unsigned int get_batch_size();
+        Batch* get_next_batch();
         ~Piler();
-        //TODO destructor that closes file if not cin
-
-        int get_m_cells();
-        Batch get_next_batch();
 
     private:
         std::istream* pileup_stream;
-        int m_cells, batch_size;
+        int m_cells;
+        unsigned int min_queue_size, max_queue_size, last_batch_id, batch_size;
+        Batch_Q batch_queue;
 };
 
 /*Eventually three possible options:
@@ -48,5 +82,8 @@ class Piler {
 //IDEA: have a list of batches ready (n=O(2*number of threads)). When initialized or batch requested, spawn a worker that works to repopulate
 //the batches until there are enough, then dies. When a main worker requests a new batch, should have a whole batch ready to go already piled.
 //?If not, dedicate more workers to piling? Will have to work on this...
+//
+//ALSO: in general, how to limit number of threads. If some thread (or many in case of in-house piling) are running in the piler, some in the other workers, how to keep total at t?
+//Some sort of shared n_current threads variable, or even a thread manager class with mutex protected modifiers for this number and methods that can spawn new workers if some available. NOTE: this is for WAAAAY later on
 
 #endif
--- src/piler.cpp
+++ src/piler.cpp
@@ -3,13 +3,114 @@
 #include "scarborsnv.h"
 #include "piler.h"
 
+/*********************************
+ * Batch methods
+ * ******************************/
+
+Batch::Batch(int batch_size, int batch_id) {
+    this->batch_id = batch_id;
+    this->batch_size = batch_size;
+    Locus_reads** data = new Locus_reads*[batch_size]; 
+
+    for (int i=0; i<batch_size; i++) {
+        data[i] = new Locus_reads;
+    }
+    this->data = data;
+
+    return;
+}
+
+Batch::~Batch() {
+    for (int i=0; i<this->batch_size; i++) {
+        delete this->data[i];
+    }
+    delete this->data;
+
+    return;
+}
+
+unsigned int Batch::get_batch_size() {
+    //TODO
+    return 0;
+
+}
+
+int Batch::get_batch_id() {
+    //TODO
+    return 0;
+
+}
+
+Locus_reads* Batch::get_locus(int i) {
+    //TODO
+    return (Locus_reads*)NULL;
+}
+
+/*********************************
+ * Batch_Q methods
+ * ******************************/
+
+Batch_Q::Batch_Q() {
+    this->max_size = -1;
+    this->read_complete = false;
+    //std::queue<Batch*> data;
+    //TODO initialize
+    //std::mutex m_queue;
+    //TODO !!!
+}
+
+void Batch_Q::set_max_size(int max_size) {
+    this->max_size = max_size;
+    return;
+}
+
+Batch* Batch_Q::pop() {
+    //TODO !!!
+        /*Returns oldest batch and deletes from queue
+         * If empty returns null pointer TODO:(?)*/
+        //TODO: mutex protected. If no batch available yet(and !read_complete), what to do? return null? Wait? reader only takes mutex when pushing complete Batch. 
+    return (Batch*) NULL;
+}
+
+void Batch_Q::push(Batch* b){
+    //TODO
+}
+
+unsigned int Batch_Q::size() {
+    //TODO mutex lock;
+    return this->n;
+}
+
+Batch_Q::~Batch_Q() {
+    //TODO
+}
+
+/*********************************
+ * Piler methods
+ * ******************************/
+
 Piler::Piler(std::istream* instream, int batch_size, bool is_bams) {
     //TODO: if_bams?
     this->batch_size = batch_size;
     this->pileup_stream = instream;
+    this->last_batch_id = -1;
+    //TODO real queue size
+    //this->batch_queue = Batch_Q(5);
+    //TODO:
     std::string test;
     getline(*instream, test);
     std::cout << test << std::endl;
+}
+
+unsigned int Piler::get_batch_size() {
+    return this->batch_size;
+}
+
+Batch* Piler::get_next_batch() {
+    if (this->batch_queue.size() < this->min_queue_size) {
+        //TODO: handle queue refillling here as necessary if queue below min size
+    }
+    return this->batch_queue.pop();
 }
 
 Piler::~Piler() {
@@ -22,9 +123,4 @@ Piler::~Piler() {
     std::cout << test << std::endl;
 }
 
-Batch::Batch(int batch_size) {
-    Cell_reads* data = new Cell_reads[batch_size]; 
-    for (int i=0; i<batch_size; i++) {
-        data[i] = Cell_reads();
-    }
-}
+
