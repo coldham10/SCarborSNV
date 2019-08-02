18ea0f1d234039b8f385ea2c41d38c499bb4730a
Christopher Oldham <coldham10@gmail.com>
2019-08-02 08:28:38 -0400

    Added homozygous cell probability

--- src/cell_likelihoods.c
+++ src/cell_likelihoods.c
@@ -29,61 +29,76 @@ int prepare_reads(Cell_locus* cell) {
 
 /*Returns log probability of amplifying to beta given the genotype
  * Using distribution from Zafar et al (2016), supplementary table 6*/
-double l_P_amplification(nuc_t ref, nuc_t beta, int g, double l_P_err) {
+long double l_P_amplification(nuc_t ref, nuc_t beta, int g, long double l_P_err) {
+    /* Since l_P_err does not change these are memoized for efficiency */
+    static long double l_P1 = NAN;
+    static long double l_P2 = NAN;
+    static long double l_P3 = NAN;
+    static long double l_P4 = NAN;
+    /*Initialize on first call */
+    if isnan(l_P1) {
+        l_P1 = logl(1 - expl(logl(3) + logl(l_P_err)));
+    }
+    if isnan(l_P2) {
+        /* log((1-2*p_e)/2) */
+        l_P2 = logl(1 - expl(logl(2) + l_P_err)) - logl(2);
+    }
+    if isnan(l_P3) {
+        /* (1/3)*(1- l_P2) */
+        l_P3 = LSE2(-1 * logl(6), l_P_err - logl(3));
+    }
+    if isnan(l_P4) {
+        l_P4 = logl(1-expl(l_P_err)) - logl(3);
+    }
+
+    /* return appropriate value */
     if (g == 0) {
-        return (beta == ref) ? log(1 - exp(log(3) + log(l_P_err))) : l_P_err;
+        return (beta == ref) ?  l_P1 : l_P_err;
     }
     else if (g == 1) {
-        if (beta == ref) {
-            /* log((1-2*p_e)/2) */
-            return log(1 - exp(log(2) + l_P_err)) - log(2);
-        }
-        else {
-            /* (1/3)*(1- the above) */
-            return (double)LSE2(-1 * log(6), l_P_err - log(3));
-        }
+        return (beta == ref) ? l_P2 : l_P3;
     }
     else if (g == 2) {
-        return (beta == ref) ? l_P_err : log(1-exp(l_P_err)) - log(3);
+        return (beta == ref) ? l_P_err : l_P4;
     }
     else {
         /*Error */
-        return -1;
+        return NAN;
     }
 }
 
 
-
-
-
 /*Homozygous likelihood calculation */
-long double homoz_cell_l(Cell_locus* cell, int g, nuc_t ref, double l_P_amp_err) {
+long double homoz_cell_l(Cell_locus* cell, int g, nuc_t ref, long double l_P_amp_err) {
     int k;
     nuc_t read;
-    long double l_e;
+    long double l_e, l_P_beta__g, term1, term2;
     /* product found by summing in log space */
     long double product = 0;
     for (k = 0; k < cell->read_count; k++;) {
         read = cell->reads[k];
         l_e  = cell->quals[k];
 
-        l_P_beta__g = l_P_amplification(ref, read, g);
-
+        l_P_beta__g = l_P_amplification(ref, read, g, l_P_amp_err);
+        term1 = logl(1-expl(l_P_amp_err)) + l_P_beta__g;
+        term2 = l_P_beta__g - logl(3) + logl(1-expl(l_P_amp_err));
+        product += LSE2(term1, term2);
     }
 
+    return product;
 }
 
 /*Heterozygous likelihood calculation */
-long double hetz_cell_l(Cell_locus* cell, int g, nuc_t ref, double l_P_amp_err) {
+long double hetz_cell_l(Cell_locus* cell, int g, nuc_t ref, long double l_P_amp_err) {
     /*TODO*/
 }
 
 /*Given a Cell_locus, log-probability of amplification error and an 3-length array for results, compute P(g|d_ij)*/
-int cell_likelihoods (Cell_locus* cell, long double* log_ls, nuc_t ref, double l_P_amp_err) {
+int cell_likelihoods (Cell_locus* cell, long double* log_ls, nuc_t ref, long double l_P_amp_err) {
     int i;
     if (cell->read_count < 1) {
-        /*Indicate insufficient depth. XXX must check log_likelihoods to be positive*/
-        log_ls[0] = log_ls[1] = log_ls[2] = -1;
+        /*Indicate insufficient depth. */
+        log_ls[0] = log_ls[1] = log_ls[2] = NAN;
         return 1;
     }
     /* g = 0,1,2*/
