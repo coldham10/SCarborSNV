0b1fcb33738302bdb820facc3203089e512f2fe5
Christopher Oldham <coldham10@gmail.com>
2019-08-10 09:03:15 -0400

    After building tree, sequencing

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -7,9 +7,12 @@ random.seed(1738)
 
 N_SITES = 10000
 CHROM_LENGTH = 1000
-HAPLOID_RATE = 0.0001
 SNV_RATE = 0.02
 BRANCH_RATE = 0.002
+HAPLOID_RATE = 0.001
+P_ADO_CHROM = 0.1
+AMP_ERR = 0.01
+SEQ_ERR = 0.01
 N_ITERS = 1400
 CLONAL_ITERS = 700
 READ_DEPTH = 10
@@ -81,7 +84,55 @@ def print_tree(T):
         print(":{})RT;".format(len(T.mutations)))
     return
 
-def sequence_site():
+def drop_alleles():
+    for cell in active_nodes:
+        n_chrms = N_SITES//CHROM_LENGTH
+        for c in range(n_chrms):
+            if np.random.uniform(0,1) < P_ADO_CHROM:
+                cell.chrmabs.append(c, np.random.randint(0,1))
+    return
+
+def sequence_site(locus, init_depth):
+    ref = np.random.choice(['A','C','G','T'])
+    alt = np.random.choice(['A','C','G','T'].remove(ref))
+    cells = []
+    for cell in active_nodes:
+        depth = init_depth + int(np.random.normal(0, np.sqrt(READ_DEPTH)))
+        if depth < 1:
+            cells.append((0, '*', '*'))
+            continue
+        genotype = [0,0]
+        if locus in cell.mutations:
+            genotype[1] = 1
+        chrm = locus//CHROM_LENGTH
+        for pos, strand in cell.chrmabs:
+            if pos//CHROM_LENGTH == chrm:
+                genotype[strand] = -1
+        genotype = [g for g in genotype if g != -1]
+        if len(genotype) == 0:
+            cells.append((0, '*', '*'))
+            continue
+        reads = []
+        quals = []
+        for j in range(depth):
+            x = ref if np.random.choice(genotype) == 0 else alt
+            if np.random.uniform(0,1) < AMP_ERR:
+                x = np.random.choice(['A','C','G','T'].remove(x))
+            seq_err = SEQ_ERR + np.random.normal(SEQ_ERR, 0.2)
+            if seq_err < 0:
+                seq_err = 0
+            if np.random.uniform(0,1) < seq_err:
+                x = np.random.choice(['A','C','G','T','N'].remove(x))
+            reads.append(x)
+            quals.append(to_phred(seq_err))
+        cells.append((depth, "".join(reads), "".join(quals)))
+    return cells
+            
+
+
+
+
+
 
 if __name__ == "__main__":
     root = Node(last_id, is_root=True);
@@ -93,7 +144,14 @@ if __name__ == "__main__":
             n.try_branch()
     label_tree()
     print_tree(root)
+    drop_alleles()
     current_depth = READ_DEPTH
     for i in range(N_SITES):
-        if i % 10 == 0:
-            current_depth += np.random.normal(0.1)
+        if i % CHROM_LENGTH == 0:
+            current_depth = READ_DEPTH
+        elif i % 10 == 0:
+            current_depth += int(np.random.normal(0,2)); 
+            if current_depth < 0:
+                current_depth = 0;
+        sequence_site(i, current_depth)
+
