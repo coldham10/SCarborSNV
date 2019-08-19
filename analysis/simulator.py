ef6e5016a5da4226af562a21e66dfe2400d0dc71
Chris Oldham <coldham10@gmail.com>
2019-08-19 09:11:22 +0200

    Started on analysis

--- analysis/simulator.py
+++ analysis/simulator.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+#  SCarborSNV: Efficient Phylogeny-aware Single Nucleotide Variant Detection for Single Cells
+# 
+#  Copyright (C) 2019 Christopher Oldham
+# 
+#  This file is part of SCarborSNV.
+# 
+#  SCarborSNV is free software: you can redistribute it and/or modify
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+# 
+#  SCarborSNV is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with SCarborSNV.  If not, see <https://www.gnu.org/licenses/>.
+
 import random
 import numpy as np
 
@@ -42,6 +61,7 @@ class Node:
             mut_site = random.randrange(0, N_SITES)
             while mut_site in mutation_sites:
                 mut_site = random.randrange(0, N_SITES)
+            #TODO add a chromosome number
             mutation_sites.append(mut_site)
             self.mutations.append(mut_site)
             return True
